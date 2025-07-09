e77869f6f438a0ab6124adfb7814ad18649b8cb2
scandiacusqiu <scandiacusqiu@gmail.com>
2025-07-09 10:50:53 +0800

    added RAW photo type

--- 图片文件批量重命名.py
+++ 图片文件批量重命名.py
@@ -1,5 +1,6 @@
 import os
 import exifread
+from tqdm import tqdm
 from tkinter.filedialog import askdirectory
 
 current_path = askdirectory()
@@ -8,29 +9,54 @@ if current_path == " ":
 
 all_files = os.listdir(current_path)
 images = []
+types = []
 dates = {}
 
 for i in all_files:  # Find all the files that is a picture
-    if i[-4:] == ".jpg" or i[-4:] == ".JPG" or i[-5:] == ".jpeg" or i[-4:] == "PNG" or i[-4:] == "png":
+    if i[-4:] == ".jpg" or i[-4:] == ".JPG" or i[-4:] == ".PNG" or i[-4:] == ".png" or i[-4:] == ".ARW":
         images.append(i)
+        types.append(i[-4:])
+    elif i[-5:] == ".jpeg":
+        images.append(i)
+        types.append(i[-5:])
 
-for i in images:    # Get the date data in each picture and store in a dictionary
-    image = current_path + "/" + i
-    fd = open(image, "rb")
-    tags = exifread.process_file(fd)
-    fd.close()
-    for tag in tags:
-        if tag == "Image DateTime":
-            dates.update({str(tags[tag])[0:10]: 1})
+file_number = len(images)
+proceed = input(str(file_number) + ' files detected, proceed? (y/n): ')
 
-for i in images:
-    image = current_path + "/" + i
-    fd = open(image, "rb")
-    tags = exifread.process_file(fd)
-    fd.close()
-    for tag in tags:
-        if tag == "Image DateTime":
-            date = str(tags[tag])[0:10]
-    new_name = str(date) + "-" + str(dates[date]) + ".jpg"
-    dates.update({date: dates[date] + 1})
-    os.rename(image, current_path + "/" + new_name)
+if proceed == 'y':
+    print('extracting file dates...')
+    pbar = tqdm(total=file_number)
+    for i in images:    # Get the date data in each picture and store in a dictionary
+        image = current_path + "/" + i
+        fd = open(image, "rb")
+        tags = exifread.process_file(fd)
+        fd.close()
+        for tag in tags:
+            if tag == "Image DateTime":
+                dates.update({str(tags[tag])[0:10]: 1})
+        pbar.update(1)
+    # pbar.close()
+    del pbar
+
+    print('dates all extracted')
+
+    print('renaming...')
+    pbar = tqdm(total=file_number)
+    index = 0
+    for i in images:
+        image = current_path + "/" + i
+        fd = open(image, "rb")
+        tags = exifread.process_file(fd)
+        fd.close()
+        for tag in tags:
+            if tag == "Image DateTime":
+                date = str(tags[tag])[0:10]
+        new_name = str(date) + " " + str(dates[date]) + types[index]
+        index += 1
+        dates.update({date: dates[date] + 1})
+        os.rename(image, current_path + "/" + new_name)
+        pbar.update(1)
+    pbar.close()
+else:
+    print("executing")
+    exit()
